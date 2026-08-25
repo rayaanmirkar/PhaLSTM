f065e204b1ab7d901535eb6afe035f04dcd41cad
iocle <ilmrexplorer@gmail.com>
2026-08-25 12:49:20 -0400

    Homology-based split and scripts

--- building_data/homology_split.py
+++ building_data/homology_split.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv("final_data_training.csv") 
-unique_genera = df['genus'].dropna().sample(frac=1, random_state=42).unique()
+df = pd.read_csv("C:/Users/raypi/coding/PhaLSTM/building_data/final_data_training.csv") 
+unique_genera = pd.Series(df['Host genus'].dropna().unique()).sample(frac=1, random_state=42).to_numpy()
 
 train_end = int(len(unique_genera) * 0.8)
 val_end = int(len(unique_genera) * 0.9)
@@ -10,10 +10,6 @@ train_genera = unique_genera[:train_end]
 val_genera = unique_genera[train_end:val_end]
 test_genera = unique_genera[val_end:]
 
-train_df = df[df['genus'].isin(train_genera)]
-val_df = df[df['genus'].isin(val_genera)]
-test_df = df[df['genus'].isin(test_genera)]
-
-train_df.to_csv("new_train_split.csv", index=False)
-val_df.to_csv("new_val_split.csv", index=False)
-test_df.to_csv("new_test_split.csv", index=False)
+df[df['Host genus'].isin(train_genera)].to_csv("new_train_split.csv", index=False)
+df[df['Host genus'].isin(val_genera)].to_csv("new_val_split.csv", index=False)
+df[df['Host genus'].isin(test_genera)].to_csv("new_test_split.csv", index=False)
